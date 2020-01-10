81c7d12f08a67962f0970b52d2c5e928a0e2fb8c
AbhijitX Roy <abhijitx.roy@intel.com>
2020-01-10 12:34:27 +0530

    TreeMap sort by value

--- InterViewGuide/src/com/java/collection/TreeMapSortByValue.java
+++ InterViewGuide/src/com/java/collection/TreeMapSortByValue.java
@@ -1,55 +1,56 @@
 package com.java.collection;
 
 import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Map.Entry;
+import java.util.SortedSet;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class TreeMapSortByValue
 {
-    static Map< Integer, String > map = new TreeMap<>( new MyComparator() );
+
+    static SortedSet entriesSortedByValues ( Map map )
+    {
+        SortedSet sortedEntries = new TreeSet( new Comparator()
+        {
+            @Override
+            public int compare ( Object o1, Object o2 )
+            {
+                Map.Entry< String, Integer > e1 = ( Map.Entry< String, Integer > ) o1;
+                Map.Entry< String, Integer > e2 = ( Map.Entry< String, Integer > ) o2;
+
+                Integer val1 = e1.getValue();
+                Integer val2 = e2.getValue();
+                 int result = val1.compareTo( val2 );
+                 return result != 0 ? result : 1;
+                //return val1.compareTo( val2 );
+            }
+        } );
+        sortedEntries.addAll( map.entrySet() );
+        return sortedEntries;
+    }
+
+    static Map< Integer, String > map;
 
     public static void main ( String[] args )
     {
-        map.put( 1, "c" );
-        map.put( 3, "z" );
-        map.put( 5, "a" );
-        map.put( 2, "f" );
-        map.put( 4, "b" );
+        Map< String, Integer > map = new TreeMap< String, Integer >();
+        map.put( "A", 3 );
+        map.put( "B", 2 );
+        map.put( "C", 3 );
 
         System.out.println( map );
-        Iterator< Entry< Integer, String > > itr = map.entrySet().iterator();
-
-        while ( itr.hasNext() )
-        {
-            System.out.println( itr.next() );
-        }
+        // prints "{A=3, B=2, C=1}"
+        System.out.println( entriesSortedByValues( map ) );
+        // prints "[C=1, B=2, A=3]"
     }
 
 }
 
-class MyComparator
-    implements Comparator< Object >
-{
-    public int compare ( Object o1, Object o2 )
-    {
-        String i1 = o1.toString();
-        String i2 = o2.toString();
-
-        Map< Integer, String > m1 = new HashMap<>();
-        if ( m1.containsKey( o1 ) )
-        {
-            String val1 = m1.get( o1 );
-            String val2 = m1.get( o2 );
-
-            return val1.compareTo( val2 );
-        }
-        else
-        {
-            return - i2.compareTo( i1 );
-        }
-
-    }
-}
+/**
+ * static < K, V extends Comparable< ? super V > > SortedSet< Map.Entry< K, V > > entriesSortedByValues ( Map< K, V > map ) { SortedSet< Map.Entry< K, V > > sortedEntries = new
+ * TreeSet< Map.Entry< K, V > >( new Comparator< Map.Entry< K, V > >() {
+ * 
+ * @Override public int compare ( Map.Entry< K, V > e1, Map.Entry< K, V > e2 ) { int res = e1.getValue().compareTo( e2.getValue() ); return res != 0 ? res : 1; } } );
+ *           sortedEntries.addAll( map.entrySet() ); return sortedEntries; }
+ */
