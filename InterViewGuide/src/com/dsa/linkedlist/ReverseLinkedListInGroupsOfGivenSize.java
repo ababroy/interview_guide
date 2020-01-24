d6cf62984947f65262052b9ba9788eae8470e734
AbhijitX Roy <abhijitx.roy@intel.com>
2020-01-24 16:08:09 +0530

    Reverse a Linked List in groups of given size

---
+++ InterViewGuide/src/com/dsa/linkedlist/ReverseLinkedListInGroupsOfGivenSize.java
@@ -0,0 +1,26 @@
+package com.dsa.linkedlist;
+
+/**
+ * Reverse a Linked List in groups of given size
+ * 
+ * @author royabhix
+ *
+ */
+public final class ReverseLinkedListInGroupsOfGivenSize
+{
+    private ReverseLinkedListInGroupsOfGivenSize ()
+    {
+    }
+    
+    public static void reverseLinkedListInGroupsOfGivenSize(Node head)
+    {
+        if( head != null)
+        {
+            
+        }
+        else
+        {
+            
+        }
+    }
+}
---
+++ InterViewGuide/src/com/java/object/Association.java
@@ -0,0 +1,21 @@
+package com.java.object;
+
+/**
+ * 
+ * @author royabhix
+ *
+ *
+ *  For instance, Abhijit is an object. 
+ *  
+ *  In Real World example, 
+ *  
+ *  Abhijit is a Person => IS-A relation ship (Inheritance) => Tightly Coupled
+ *  
+ *  Abhijit has two Legs => Has-A relationship (Strong Association) => Composition
+ *  
+ *  Abhijit has two shirts => Has-A relationship (Weak Association) +> Aggregation
+ */
+public class Association
+{
+
+}
